179abdf1c20be626142366f1b84d11fe9a15440e
KanaiYukiha <107377086+Yukiha-ka@users.noreply.github.com>
2022-07-05 15:17:38 +0900

    set2

--- tmp.py
+++ tmp.py
@@ -1,5 +1,6 @@
 string1_y = 0
 bpm = 120
+tx = ""
 
 def drawline():
     stroke(140)
@@ -10,13 +11,28 @@ def setup():
     background(0)
     size(360,640)
     frameRate(60)
-
+   
 
 def draw():
-    global string1_y,bpm
+    global string1_y,bpm,tx
     background(0)
     drawline()
     noStroke()
     fill(255)
-    string1_y += (bpm/70)
+    string1_y +=  (bpm/70)
     ellipse(180,string1_y,50,50)
+    print(tx)
+    
+def  mouseClicked():
+    global tx
+    if 500 <= string_y <= 520:
+         #textSize(30)
+         #fill(255,0,0)
+         #text("Perfect!",180,520)
+         tx = "Perfect"
+    else:
+        #textSize(30)
+        #fill(255,0,0)
+        #text("False!",180,520)
+        tx = "False"
+    
